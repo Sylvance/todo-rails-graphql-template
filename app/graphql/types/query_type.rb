b67776e68d85a19522bc9aeaf3820b90ecc164c9
Sylvance Mbaka <kerandisylvance@gmail.com>
2020-05-04 22:18:57 +0300

    Add single todo query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,6 +4,14 @@ module Types
           [Types::TodoType],
           null: false
 
+    field :todo, Types::TodoType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def todo(id:)
+      Todo.find_by(id: id)
+    end
+
     def todos
       Todo.all
     end
